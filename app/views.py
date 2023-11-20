0971c2d16463228cbd935cad437a149a0c8331c9
veera4133 <katamveerasudheerkumar@gmail.com>
2023-11-20 22:55:55 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def sudheer(request):
+    return render(request,'sudheer.html')
+
+
+def raju(request):
+    return render(request,'raju.html')
+
+def sudheer2(request):
+    return render(request,'sudheer2.html')
